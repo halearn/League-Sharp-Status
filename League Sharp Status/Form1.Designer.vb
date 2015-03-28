54710db339c3d262a99b881bc7c0480ba8cfba62
Saeed Suleiman <halearn@gmail.com>
2015-03-28 17:28:45 +0300

    A Simple App That Checks League Of Sharp's Status

---
+++ League Sharp Status/Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.WebBrowser2 = New System.Windows.Forms.WebBrowser()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer4 = New System.Windows.Forms.Timer(Me.components)
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 0)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.ScriptErrorsSuppressed = True
+        Me.WebBrowser1.ScrollBarsEnabled = False
+        Me.WebBrowser1.Size = New System.Drawing.Size(417, 186)
+        Me.WebBrowser1.TabIndex = 0
+        '
+        'WebBrowser2
+        '
+        Me.WebBrowser2.Location = New System.Drawing.Point(0, 285)
+        Me.WebBrowser2.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser2.Name = "WebBrowser2"
+        Me.WebBrowser2.ScriptErrorsSuppressed = True
+        Me.WebBrowser2.Size = New System.Drawing.Size(93, 57)
+        Me.WebBrowser2.TabIndex = 1
+        Me.WebBrowser2.Visible = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 8000
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 1000
+        '
+        'Timer3
+        '
+        Me.Timer3.Enabled = True
+        Me.Timer3.Interval = 2000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 18.0!)
+        Me.Label1.Location = New System.Drawing.Point(6, 108)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(399, 29)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "joduska is validating your ip address"
+        '
+        'Timer4
+        '
+        Me.Timer4.Interval = 1000
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(330, 82)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "About"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(417, 186)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.WebBrowser2)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "League Sharp Status , Code & Design: Saeed , Idea : Wiciaki"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents WebBrowser1 As System.Windows.Forms.WebBrowser
+    Friend WithEvents WebBrowser2 As System.Windows.Forms.WebBrowser
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Timer2 As System.Windows.Forms.Timer
+    Friend WithEvents Timer3 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Timer4 As System.Windows.Forms.Timer
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ League Sharp Status/Form1.vb
@@ -0,0 +1,95 @@
+﻿Public Class Form1
+    Dim source As String
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        WebBrowser2.Navigate("https://www.joduska.me")
+
+
+        'Dim sourceString As String = New System.Net.WebClient().DownloadString("https://www.joduska.me")
+        ' MsgBox(sourceString)
+        ' WebBrowser1.DocumentText = sourceString
+
+        '   WebBrowser1.DocumentText = "<strong class='bbc'>LeagueSharp status: <span style='color: #FF0000'>Outdated for 5.6</span></strong> | <strong class='bbc'>Information about the auth system: </strong><a "
+    End Sub
+
+    Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
+
+    End Sub
+
+    Private Sub WebBrowser2_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser2.DocumentCompleted
+        Dim htmlSourceCode As String = WebBrowser2.DocumentText.ToString()
+
+        Dim bodyContent As String = WebBrowser2.Document.Body.InnerText
+        If bodyContent.Contains("checking") Then
+            Timer1.Start()
+            Label1.Text = ("joduska is validating your ip address")
+
+        Else
+            Label1.Text = ("Extracting Data")
+            Timer2.Start()
+        End If
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Timer1.Stop()
+        Form1_Load(Nothing, Nothing)
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Try
+            WebBrowser2.Document.GetElementById("ips_username").SetAttribute("value", "username")
+            WebBrowser2.Document.GetElementById("ips_password").SetAttribute("value", "password")
+            WebBrowser2.Document.GetElementById("lbtn").InvokeMember("click")
+        Catch
+
+        End Try
+        Dim htmlSourceCode As String = WebBrowser2.DocumentText.ToString()
+        If htmlSourceCode = "" Then
+
+        Else
+            source = htmlSourceCode
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+        MsgBox(source)
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        Try
+            If source = "" Then
+
+            Else
+                Dim index As Integer = source.IndexOf("<strong class")
+                Dim st As String = source.Substring(index, 200)
+                Dim subt = st.Split("|")
+                Dim status As String = subt(0)
+                ' MsgBox(status)
+                WebBrowser1.DocumentText = status
+                Timer3.Stop()
+                Timer1.Stop()
+                Timer2.Stop()
+                timer4.start()
+                Label1.Text = ("Data Extracted ^")
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Dim count As Integer
+    Private Sub Timer4_Tick(sender As Object, e As EventArgs) Handles Timer4.Tick
+        count += 1
+        If count = 10 Then
+            Process.Start(Application.StartupPath & "\" & Application.ProductName & ".exe")
+            Me.Close()
+        Else
+
+        End If
+
+        Label1.Text = "Data Extracted ^" & vbNewLine & "Updating Info In " & 10 - count
+    End Sub
+
+    Private Sub Button1_Click_1(sender As Object, e As EventArgs) Handles Button1.Click
+        MsgBox("Coded & Designed By Saeed Suleiman , Idea By Wiciaki")
+    End Sub
+End Class
